7f2f1c0bbbcf8ea022f6632bdfe27994ffc31d2e
Bibek <bibekswain7555@gmail.com>
2024-10-10 12:12:20 +0530

    Fix bugs in userAuthorization.js and refressAllTokens

--- api/src/middlewares/userAuthorization.js
+++ api/src/middlewares/userAuthorization.js
@@ -7,35 +7,64 @@ import { Student } from "../model/student.model.js";
 
 export const verifyJwt  = asyncHandler(async(req, res, next) => {
     try {
-        const token = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-    
-        if (!token) {
+        const atoken = req.cookies.accessToken || req.header("Authorization")?.replace("Bearer ", "")  
+        const rtoken = req.cookies.refreshToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if (!atoken && !rtoken) {
             console.log("No token found in verifyJwt")
             throw new ApiError(404, "Unauthorized request (Error from verifyjwt)")
         }
     
-        const decodedToken = jwt.decode(token, process.env.ACCESS_TOKEN_SECRET)
-        if (!decodedToken) {
-            throw new ApiError(400, "Invalid token (Error from verifyjwt)")
-        }
-        if(decodedToken.role === "student") {
-            const user = await Student.findById(decodedToken._id).select("-password -refreshToken")
-            if (!user) {
-                throw new ApiError(400, "User not Found (Error from verifyjwt)")
+        if (atoken) {
+            const decodedToken = jwt.decode(atoken, process.env.ACCESS_TOKEN_SECRET)
+            if (!decodedToken) {
+                throw new ApiError(400, "Invalid token A(Error from verifyjwt)")
             }
-            req.user = user;
-            next();
-            return
-        } else if(decodedToken.role === "admin" || decodedToken.role === "accountant") {
-            const user = await Staff.findById(decodedToken._id).select("-password -refreshToken")
-            if (!user) {
-                throw new ApiError(400, "User not Found (Error from verifyjwt)")
+            if(decodedToken.role === "student") {
+                const user = await Student.findById(decodedToken._id).select("-password -refreshToken")
+                if (!user) {
+                    throw new ApiError(400, "User not Found A(Error from verifyjwt)")
+                }
+                req.user = user;
+                next();
+                return
+            } else if(decodedToken.role === "admin" || decodedToken.role === "accountant") {
+                const user = await Staff.findById(decodedToken._id).select("-password -refreshToken")
+                if (!user) {
+                    throw new ApiError(400, "User not Found A(Error from verifyjwt)")
+                }
+                req.user = user;
+                next();
+                return
+            } else {
+                throw new ApiError(400, "Invalid token A(Error from verifyjwt)")
             }
-            req.user = user;
-            next();
-            return
+        // Added below ELSE condition to check for refershToken if the accessToken has expired
+        //So that when we try to access the protected route to refershAllTokens , verifyJwt won't block it.
         } else {
-            throw new ApiError(400, "Invalid token (Error from verifyjwt)")
+            const decodedToken = jwt.decode(rtoken, process.env.REFRESH_TOKEN_SECRET)
+            if (!decodedToken) {
+                throw new ApiError(400, "Invalid token R(Error from verifyjwt)")
+            }
+            if(decodedToken.role === "student") {
+                const user = await Student.findById(decodedToken._id).select("-password -refreshToken")
+                if (!user) {
+                    throw new ApiError(400, "User not Found R(Error from verifyjwt)")
+                }
+                req.user = user;
+                next();
+                return
+            } else if(decodedToken.role === "admin" || decodedToken.role === "accountant") {
+                const user = await Staff.findById(decodedToken._id).select("-password -refreshToken")
+                if (!user) {
+                    throw new ApiError(400, "User not Found R(Error from verifyjwt)")
+                }
+                req.user = user;
+                next();
+                return
+            } else {
+                throw new ApiError(400, "Invalid token R(Error from verifyjwt)")
+            }
         }
     } catch (error) {
         console.log(error);
